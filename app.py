3aab2187c54d6fefdf422bd4bdb0a1401c160e1f
sharukphoenix <sharukphoenox@gmail.com>
2025-02-04 22:37:17 +0530

    Added 3 new features - i) Marking a todo as complete; ii) Updating an exiting todo - added edit.html for the frontend; iii) Deleting a todo

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# app.py
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -35,5 +34,34 @@ def add():
     db.session.commit()
     return redirect(url_for('index'))
 
+# Mark a Todo as Completed
+@app.route('/complete/<int:todo_id>')
+def complete(todo_id):
+    todo = Todo.query.get(todo_id)
+    if todo:
+        todo.completed = True
+        db.session.commit()
+    return redirect(url_for('index'))
+
+# Edit an Existing Todo
+@app.route('/edit/<int:todo_id>', methods=['GET', 'POST'])
+def edit(todo_id):
+    todo = Todo.query.get(todo_id)
+    if request.method == 'POST':
+        todo.title = request.form['title']
+        todo.description = request.form['description']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('edit.html', todo=todo)
+
+# Delete a Todo
+@app.route('/delete/<int:todo_id>')
+def delete(todo_id):
+    todo = Todo.query.get(todo_id)
+    if todo:
+        db.session.delete(todo)
+        db.session.commit()
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000,debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
